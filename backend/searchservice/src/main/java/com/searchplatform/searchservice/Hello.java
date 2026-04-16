61965a0687c3f14748373feb40a7245bf1e2abe8
shanmukha raghavendra <shanmukharaghavendra.r@gmail.com>
2026-04-16 23:45:03 +0530

    initial commit

---
+++ backend/searchservice/src/main/java/com/searchplatform/searchservice/Hello.java
@@ -0,0 +1,15 @@
+package com.searchplatform.searchservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+
+    @GetMapping
+    public String hello(){
+        return "hello world";
+    }
+
+
+}
